539b04312373612da73cd4f9ffa879c724a1cd91
Scott Doxey <neogeek0101@gmail.com>
2019-04-30 14:49:22 -0400

    Lesson 4.

--- lessons/04-effects/exercise/NewPost.js
+++ lessons/04-effects/exercise/NewPost.js
@@ -1,25 +1,42 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { FaDumbbell } from 'react-icons/fa'
+import React, { useState, useEffect, useRef } from "react"
+import { FaDumbbell } from "react-icons/fa"
 
-import { useAppState } from 'app/app-state'
-import { formatDate, DATE_FORMAT } from 'app/utils'
-import Avatar from 'app/Avatar'
-import Minutes from 'app/Minutes'
-import RecentPostsDropdown from 'app/RecentPostsDropdown'
+import { useAppState } from "app/app-state"
+import { formatDate, DATE_FORMAT } from "app/utils"
+import Avatar from "app/Avatar"
+import Minutes from "app/Minutes"
+import RecentPostsDropdown from "app/RecentPostsDropdown"
 
 const MAX_MESSAGE_LENGTH = 200
 
 export default function NewPost({ takeFocus, date, onSuccess, showAvatar }) {
   const [{ auth }] = useAppState()
-  const [message, setMessage] = useState('Ran around the lake.')
+  var key = makeNewPostKey(date)
+  const [message, setMessage] = useState("")
   const messageTooLong = message.length > MAX_MESSAGE_LENGTH
 
   function handleMessageChange(event) {
     setMessage(event.target.value)
   }
 
+  useEffect(() => {
+    setMessage(getLocalStorageValue(key) || "")
+  }, [key])
+
+  useEffect(() => {
+    setLocalStorage(key, message)
+  }, [key, message])
+
+  const newPostRef = useRef()
+
+  useEffect(() => {
+    if (takeFocus) {
+      newPostRef.current.focus()
+    }
+  })
+
   return (
-    <div className={'NewPost' + (messageTooLong ? ' NewPost_error' : '')}>
+    <div className={"NewPost" + (messageTooLong ? " NewPost_error" : "")}>
       {showAvatar && <Avatar uid={auth.uid} size={70} />}
       <form className="NewPost_form">
         <textarea
@@ -27,6 +44,7 @@ export default function NewPost({ takeFocus, date, onSuccess, showAvatar }) {
           placeholder="Tell us about your workout!"
           value={message}
           onChange={handleMessageChange}
+          ref={newPostRef}
         />
         <div className="NewPost_char_count">
           {message.length}/{MAX_MESSAGE_LENGTH}
